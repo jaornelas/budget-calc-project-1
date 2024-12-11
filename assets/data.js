56a68b495b9114163bce7284b666aa02c62454e1
NeOpoku <imanimoss17@gmail.com>
2024-12-10 20:25:22 -0500

    added a new file and modified two files

---
+++ assets/data.js
@@ -0,0 +1,12 @@
+function getData() {
+    return [
+      { type: "House", count: 15349 },
+      { type: "Bungalow", count: 1656 },
+      { type: "Converted Maisonette", count: 2147 },
+      { type: "Low Rise Flats", count: 4954 },
+      { type: "Medium Rise Flats", count: 1887 },
+      { type: "High Rise Flats", count: 820 },
+      { type: "Dwelling", count: 610 },
+      { type: "Other", count: 2147 },
+    ];
+  }
